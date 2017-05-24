a9dd45edf1442d5d4af1fb83340e7de84503dfc9
Luis Arriola <luis.arriola@adelaide.edu.au>
2017-05-24 14:06:25 +0930

    Natos Function
    R script with a function created by nato

---
+++ R_Scripts_in_development/Nato_function.R
@@ -0,0 +1,16 @@
+#Function "sumamatrices" takes a matrix and performs 4 things on it
+Suma_Matrices <- function(windowsize, inputseq)
+{
+  starts <- seq(1, length(inputseq)-windowsize, by = windowsize)
+  n <- length(starts)
+  chunkbps <- numeric(n)
+  chunkstats <- numeric(n)
+  for (i in 1:n){
+    chunk <- inputseq[starts[i]:(starts[i]+windowsize-1)] 
+    chunkmean <- mean(chunk)
+    chunkstdv <- sd(chunk)
+    chunkbps[i] <- chunkmean
+    chunkstats[i] <- chunkstdv
+  }
+  return (list(starts,chunkbps,chunkstats))
+}
