d1ce87d2f6bda3d4291cec467510aee43e6df2ad
Luis Arriola <luis.arriola@adelaide.edu.au>
2017-05-24 14:09:03 +0930

    Update on PSSM
    make it more general and changed the name of one of the input files

--- 00_In_development/BWApssm.sh
+++ 00_In_development/BWApssm.sh
@@ -3,10 +3,10 @@ REF=/Volumes/ACADretina1_Backup/CHAPTER2_DATA/SMUT_GENOME/SmutansUA159.fasta
 for FQZ in ./*fastq.gz
 do
 #FQ=`basename $FQZ`
-/Users/Annarielh/bin_app/bwa-pssm/bwa aln -t 16 $REF $FQZ | /Users/Annarielh/bin_app/bwa-pssm/bwa samse $REF - $(basename "${FQZ/\.fastq\.gz/}") \
+/Users/Annarielh/bin_app/bwa-pssm/bwa aln -t 16 $REF $FQZ | /Users/Annarielh/bin_app/bwa-pssm/bwa samse $REF - $(basename $FQZ) \
 | samtools view -uSh - \
 | samtools sort - $(basename "${FQZ/\.fastq\.gz/}")_bwapssm.sort
 done
-for BAM in *_bwapssm.sort.bam ; do samtools index $BAM & done
-for BAM in *_bwapssm.sort.bam ; do samtools flagstat $BAM > ${BAM}.stats & done
+for BAM in *.sort.bam ; do samtools index $BAM & done
+for BAM in *.sort.bam ; do samtools flagstat $BAM > ${BAM}.stats & done
 wait
