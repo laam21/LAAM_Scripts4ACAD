2e911ef2444234cd154624c27ceb0bd7c97082c4
Luis Arriola <luis.arriola@adelaide.edu.au>
2017-05-29 15:46:51 +0930

    Update to new directory
    -Changed path for directories
    -Changed number of cores
    -Changed fastq to fasta

--- 00_In_development/BWApssm.sh
+++ 00_In_development/BWApssm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-REF=/Volumes/ACADretina1_Backup/CHAPTER2_DATA/SMUT_GENOME/SmutansUA159.fasta
-for FQZ in ./*fastq.gz
+REF=/Volumes/ACADretina1_Backup/PHD/CHAPTER2_DATA/SMUTANS_MAPPING_TEST/SMUT_GENOME/SmutansUA159.fasta
+for FQZ in ./*fasta.gz
 do
 #FQ=`basename $FQZ`
-/Users/Annarielh/bin_app/bwa-pssm/bwa aln -t 16 $REF $FQZ | /Users/Annarielh/bin_app/bwa-pssm/bwa samse $REF - $(basename $FQZ) \
+/Users/Annarielh/bin_app/bwa-pssm/bwa aln -t 2 $REF $FQZ | /Users/Annarielh/bin_app/bwa-pssm/bwa samse $REF - $(basename $FQZ) \
 | samtools view -uSh - \
-| samtools sort - $(basename "${FQZ/\.fastq\.gz/}")_bwapssm.sort
+| samtools sort - $(basename "${FQZ/\.fasta\.gz/}")_bwapssm.sort
 done
 for BAM in *.sort.bam ; do samtools index $BAM & done
 for BAM in *.sort.bam ; do samtools flagstat $BAM > ${BAM}.stats & done
