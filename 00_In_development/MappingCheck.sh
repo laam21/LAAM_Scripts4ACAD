9b554eec3b1dd080484549c115b374c1956f7720
Luis Arriola <luis.arriola@adelaide.edu.au>
2017-06-27 18:20:46 +0930

    Version 0.1
    Checks for different Mapping Qualities

--- 00_In_development/MappingCheck.sh
+++ 00_In_development/MappingCheck.sh
@@ -5,31 +5,30 @@
 #
 #       Luis Arriola
 #       Last Update: 19 June 2017
-#               v.0
+#               v.0.1
 #
 ##########################################################
 #
 # This script checks how many reads were mapped within the correct slidding window
+### sort -k4,4 SIM_50.readStartIndexes_COORD_HEADER.bed >  SIM_50.readStartIndexes_COORD_HEADER.SORT4.bed
 
 #Produce BEDfiles from BAMs
-#bedtools bamtobed -i SIM_150.DAMAGED.0_bwapssm.sort.bam | head
-#bedtools bamtobed -i SIM_150.DAMAGED.0_bwapssm.sort.QM0.bam | join -1 4 -2 4 /Volumes/ACADretina1_Backup/PHD/CHAPTER2_DATA/SMUTANS_MAPPING_TEST/1_Input_Datasets/SMUT_GENOME/0_AllDatasets/Ground_Truth/SIM_150.readStartIndexes_COORD_HEADER.bed /dev/stdin
-#Join file with original coordinates  with the mappedReads Bedfiles  and calculate
-join -1 4 -2 4 file1 file2 |awk '
+for MQUAL in 0 5 10 15 20 25 30 35 40 ; do \
+samtools view -q $MQUAL -bh -F0x4 SIM_150.DAMAGED.0_bwapssm.sort.bam \
+| bedtools bamtobed -i /dev/stdin \
+| sort -k4,4 \
+| join -1 4 -2 4 SIM_150.readStartIndexes_COORD_HEADER.SORT4.bed /dev/stdin | awk -v MQ=$MQUAL '
 BEGIN{
-  print "HEADER\tChrOri\tStOri\tEndOri\tChrMap\tStMap\tEndMap\tStrand\tMAP";
   TrueCount=0;
   FalseCount=0;
 }
 {
   if((($3-1) <= $6) && (($4-1)>=$7)){
     TrueCount++;
-    print $0"\tTRUE\t"$3-1-$6"\t"$4-1-$7;
   }else{
     FalseCount++;
-    print $0"\tFALSE\t"$3-1-$6"\t"$4-1-$7;
   }
 }
 END{
-  print TrueCount"\t"FalseCount
-} '
+  print MQ"\t"TrueCount"\t"FalseCount
+} ' ; done
