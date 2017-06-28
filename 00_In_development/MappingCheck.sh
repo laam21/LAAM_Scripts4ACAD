03fbbbf73bc36954ecc27e183a1af24f248a6562
Luis Arriola <luis.arriola@adelaide.edu.au>
2017-06-28 13:18:37 +0930

    v.0.1
    The script now takes bam files and checks the size of the read and
    tests against its ground truth files

--- 00_In_development/MappingCheck.sh
+++ 00_In_development/MappingCheck.sh
@@ -13,22 +13,34 @@
 ### sort -k4,4 SIM_50.readStartIndexes_COORD_HEADER.bed >  SIM_50.readStartIndexes_COORD_HEADER.SORT4.bed
 
 #Produce BEDfiles from BAMs
-for MQUAL in 0 5 10 15 20 25 30 35 40 ; do \
-samtools view -q $MQUAL -bh -F0x4 SIM_150.DAMAGED.0_bwapssm.sort.bam \
-| bedtools bamtobed -i /dev/stdin \
-| sort -k4,4 \
-| join -1 4 -2 4 SIM_150.readStartIndexes_COORD_HEADER.SORT4.bed /dev/stdin | awk -v MQ=$MQUAL '
-BEGIN{
-  TrueCount=0;
-  FalseCount=0;
-}
-{
-  if((($3-1) <= $6) && (($4-1)>=$7)){
-    TrueCount++;
-  }else{
-    FalseCount++;
-  }
-}
-END{
-  print MQ"\t"TrueCount"\t"FalseCount
-} ' ; done
+for BAM in *.bam ; do
+  if [ $(echo $BAM | cut -d"." -f1 | cut -d"_" -f2) == 50 ]; then
+    REF="SIM_50.readStartIndexes_COORD_HEADER.SORT4.bed";
+  elif [ $(echo $BAM | cut -d"." -f1 | cut -d"_" -f2) == 75 ]; then
+    REF="SIM_75.readStartIndexes_COORD_HEADER.SORT4.bed";
+  else
+    REF="SIM_150.readStartIndexes_COORD_HEADER.SORT4.bed";
+  fi;
+
+  for MQUAL in 0 5 10 15 20 25 30 35 40 ; do
+    samtools view -q $MQUAL -bh -F0x4 $BAM \
+    | bedtools bamtobed -i /dev/stdin \
+    | sort -k4,4 \
+    | join -1 4 -2 4 $REF /dev/stdin \
+    | awk -v MQ=$MQUAL -v FILE=$BAM '
+        BEGIN{
+          TrueCount=0;
+          FalseCount=0;
+        }
+        {
+          if((($3-1) <= $6) && (($4-1)>=$7)){
+            TrueCount++;
+          }else{
+            FalseCount++;
+          }
+        }
+        END{
+          print FILE"\t"MQ"\t"TrueCount"\t"FalseCount"\t"TrueCount+FalseCount"\t"
+        } ';
+    done
+done
